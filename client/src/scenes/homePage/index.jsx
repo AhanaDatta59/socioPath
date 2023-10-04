ab2944e112558ef08967ed1870fecd5c011b4032
Ahana Datta <116972422+AhanaDatta59@users.noreply.github.com>
2023-10-05 00:59:25 +0530

    Design, HomePage  and Dark-Light Mode completed

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from 'scenes/navbar';
+import { Box } from '@mui/material';
+
+const HomePage = () => {
+  return (
+    <Box>
+      <Navbar/>
+    </Box>
+   // <div>HomePage</div>
+  );
+};
+
+export default HomePage;
